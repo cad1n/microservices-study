7887c556c3b3f90261a5f7f3c7716916a65792fe
Caio Augusto <caioaugusto4520@gmail.com>
2023-06-22 17:18:40 -0300

    Add Feign Client configuration

--- hr-payroll/src/main/java/microservices/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/microservices/hrpayroll/services/PaymentService.java
@@ -2,29 +2,25 @@ package microservices.hrpayroll.services;
 
 import microservices.hrpayroll.entities.Payment;
 import microservices.hrpayroll.entities.Worker;
+import microservices.hrpayroll.feignclients.WorkerFeignClient;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 @Service
 public class PaymentService {
 
-    @Value("${hr-worker.host}")
-    private String workerHost;
-
     @Autowired
-    private RestTemplate restTemplate;
+    private WorkerFeignClient workerFeignClient;
 
     public Payment getPayment(long workerId, int days) {
         Map<String, String> uriVariables = new HashMap<>();
         uriVariables.put("id", String.valueOf(workerId));
 
-        Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
-        assert worker != null;
-        return new Payment(worker.getName(), worker.getDailyIncome(), days);
+        Worker worker = workerFeignClient.findById(workerId).getBody();
+        return new Payment(Objects.requireNonNull(worker).getName(), worker.getDailyIncome(), days);
     }
 }
