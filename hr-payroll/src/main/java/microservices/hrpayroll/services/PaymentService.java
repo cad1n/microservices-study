922c4f394339ea847a89469e2c371456dfe2185b
Caio Augusto <caioaugusto4520@gmail.com>
2023-07-05 01:32:06 -0300

    Ribbon load balance configuration

--- hr-payroll/src/main/java/microservices/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/microservices/hrpayroll/services/PaymentService.java
@@ -5,20 +5,15 @@ import microservices.hrpayroll.entities.Worker;
 import microservices.hrpayroll.feignclients.WorkerFeignClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Objects;
 
 @Service
 public class PaymentService {
 
     @Autowired
-    private WorkerFeignClient workerFeignClient;
+   private WorkerFeignClient workerFeignClient;
 
     public Payment getPayment(long workerId, int days) {
-        Map<String, String> uriVariables = new HashMap<>();
-        uriVariables.put("id", String.valueOf(workerId));
 
         Worker worker = workerFeignClient.findById(workerId).getBody();
         return new Payment(Objects.requireNonNull(worker).getName(), worker.getDailyIncome(), days);
--- hr-worker/src/main/java/microservices/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/microservices/hrworker/resources/WorkerResource.java
@@ -2,7 +2,10 @@ package microservices.hrworker.resources;
 
 import microservices.hrworker.entities.Worker;
 import microservices.hrworker.repositories.WorkerRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,17 +18,25 @@ import java.util.List;
 @RequestMapping(value = "/workers")
 public class WorkerResource {
 
+    private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
+
+    @Autowired
+    private Environment env;
+
     @Autowired
     private WorkerRepository repository;
 
     @GetMapping
-    private ResponseEntity<List<Worker>> findAll() {
+    public ResponseEntity<List<Worker>> findAll() {
         List<Worker> list = repository.findAll();
         return ResponseEntity.ok(list);
     }
 
     @GetMapping(value = "/{id}")
-    private ResponseEntity<Worker> findById(@PathVariable Long id) {
+    public ResponseEntity<Worker> findById(@PathVariable Long id) {
+
+        logger.info("PORT = " + env.getProperty("local.server.port"));
+
         Worker obj = repository.findById(id).stream().findFirst().orElse(null);
         return ResponseEntity.ok(obj);
     }
