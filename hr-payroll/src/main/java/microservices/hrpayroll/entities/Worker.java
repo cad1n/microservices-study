166e70e70398ddffe05e75cd9b152065631fd3e2
Caio Augusto <caioaugusto4520@gmail.com>
2023-07-07 23:08:11 -0300

    Add hr-eureka-server app

--- hr-payroll/src/main/java/microservices/hrpayroll/entities/Worker.java
+++ hr-payroll/src/main/java/microservices/hrpayroll/entities/Worker.java
@@ -1,12 +1,16 @@
 package microservices.hrpayroll.entities;
 
+import lombok.*;
+
 import java.io.Serial;
 import java.io.Serializable;
-import java.util.Objects;
 
-
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class Worker implements Serializable {
-
 
     @Serial
     private static final long serialVersionUID = 1L;
@@ -16,50 +20,4 @@ public class Worker implements Serializable {
     private String name;
 
     private Double dailyIncome;
-
-    public Worker() {
-    }
-
-    public Worker(Long id, String name, Double dailyIncome) {
-        this.id = id;
-        this.name = name;
-        this.dailyIncome = dailyIncome;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Double getDailyIncome() {
-        return dailyIncome;
-    }
-
-    public void setDailyIncome(Double dailyIncome) {
-        this.dailyIncome = dailyIncome;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Worker worker = (Worker) o;
-        return Objects.equals(id, worker.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
 }
