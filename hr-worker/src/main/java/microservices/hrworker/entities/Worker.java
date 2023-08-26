92eb649c62c9b71d3421b50692ec47cc0242fa1f
cad1n <caioaugusto4520@gmail.com>
2023-08-26 00:32:58 -0300

    Dependency update

--- hr-worker/src/main/java/microservices/hrworker/entities/Worker.java
+++ hr-worker/src/main/java/microservices/hrworker/entities/Worker.java
@@ -1,10 +1,24 @@
 package microservices.hrworker.entities;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.io.Serial;
 import java.io.Serializable;
-import java.util.Objects;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
 @Entity
 @Table(name = "TB_WORKER")
 public class Worker implements Serializable {
@@ -19,51 +33,4 @@ public class Worker implements Serializable {
     private String name;
 
     private Double dailyIncome;
-
-    public Worker() {
-
-    }
-
-    public Worker(Long id, String name, Double dailyIncome) {
-        this.id = id;
-        this.name = name;
-        this.dailyIncome = dailyIncome;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Double getDailyIncome() {
-        return dailyIncome;
-    }
-
-    public void setDailyIncome(Double dailyIncome) {
-        this.dailyIncome = dailyIncome;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Worker worker = (Worker) o;
-        return Objects.equals(id, worker.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
 }
